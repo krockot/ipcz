b0b82b2c470b37e621421cd07eefed007c82b519
Ken Rockot <rockot@google.com>
2021-12-18 17:16:38 -0800

    add documentation to node.h

--- src/core/node.h
+++ src/core/node.h
@@ -25,10 +25,22 @@ namespace core {
 class NodeLink;
 class Portal;
 
+// A Node controls creation and interconnection of a collection of routers which
+// can establish links to and from other routers in other nodes. Every node is
+// assigned a globally unique name by a trusted broker node, and nodes may be
+// introduced to each other exclusively through such brokers.
 class Node : public mem::RefCounted {
  public:
   enum class Type {
+    // A broker node assigns its own name and is able to assign names to other
+    // nodes upon connection. Brokers are trusted to introduce nodes to each
+    // other upon request, and brokers may connect to other brokers in order to
+    // share information and effectively bridge two node networks together.
     kBroker,
+
+    // A "normal" (i.e. non-broker) node is assigned a permanent name by the
+    // first broker node it connects to, and it can only make contact with other
+    // nodes by requesting an introduction from that broker.
     kNormal,
   };
 
@@ -37,30 +49,69 @@ class Node : public mem::RefCounted {
   Type type() const { return type_; }
   const IpczDriver& driver() const { return driver_; }
 
+  // Deactivates all NodeLinks and their underlying driver transports in
+  // preparation for this node's imminent destruction.
   void ShutDown();
+
+  // Connects to another node using `driver_transport` for I/O to and from the
+  // other node. If the caller has a handle to the process which hosts the
+  // remote node, it may be provided by `remote_process`. `initial_portals` is
+  // a collection of new portals who will immediately begin to route parcels
+  // over a link to the new node, assuming the link is established successfully.
   IpczResult ConnectNode(IpczDriverHandle driver_transport,
                          Type remote_node_type,
                          os::Process remote_process,
                          absl::Span<IpczHandle> initial_portals);
+
+  // Opens a new pair of directly linked portals on this node and returns
+  // references to both of them.
   Portal::Pair OpenPortals();
 
+  // Looks up a NodeLink by name. If there's no known link to the named node,
+  // this returns null.
   mem::Ref<NodeLink> GetLink(const NodeName& name);
 
+  // Asynchronously establishes a NodeLink to the named node and invokes
+  // `callback` when complete. If it's determined that establishing a link won't
+  // be possible, `callback` may be invoked with null.
+  //
+  // If this node already has a link to the named node, `callback` may be
+  // invoked with that link synchronously before EstablishLink() returns. If
+  // an introduction request is already in flight for the named node, `callback`
+  // is stored and will be invoked one the introduction is complete. Otherise,
+  // this node will send an introduction request to its broker before returning
+  // and `callback` will be invoked once that request is completed.
   using EstablishLinkCallback = std::function<void(NodeLink*)>;
   void EstablishLink(const NodeName& name, EstablishLinkCallback callback);
 
+  // Handles an incoming introduction request. This message is only accepted by
+  // broker nodes. If the broker knows of the node named in `request`, it will
+  // send an IntroduceNode message to both that node, and the node which sent
+  // this request (at `front_node_link`), with a new driver transport for each
+  // one which connects them each other.
   bool OnRequestIntroduction(NodeLink& from_node_link,
                              const msg::RequestIntroduction& request);
+
+  // Handles an incoming introduction to the named node. If `known` is false,
+  // then introduction has failed and the rest of the parameters can be ignored.
+  // Otherwise `link_buffer_memory` establishes an initial memory buffer shared
+  // between the nodes, and `serialized_transport_data` and
+  // `serialized_transport_handles` comprise the complete serialized
+  // representation of a new driver transport which can be used for I/O to and
+  // from the named node.
   bool OnIntroduceNode(const NodeName& name,
                        bool known,
                        os::Memory link_buffer_memory,
                        absl::Span<const uint8_t> serialized_transport_data,
                        absl::Span<os::Handle> serialized_transport_handles);
+
+  // Handles an incoming request to bypass a proxying router on another node.
   bool OnBypassProxy(NodeLink& from_node_link, const msg::BypassProxy& bypass);
 
  private:
   ~Node() override;
 
+  // Registers a new NodeLink for the given `remote_node_name`.
   bool AddLink(const NodeName& remote_node_name, mem::Ref<NodeLink> link);
 
   const Type type_;
@@ -68,10 +119,26 @@ class Node : public mem::RefCounted {
   const IpczDriverHandle driver_node_;
 
   absl::Mutex mutex_;
+
+  // The name assigned to this node by the first broker it connected to. Once
+  // assigned, this name remains constant through the life of the node.
   NodeName assigned_name_ ABSL_GUARDED_BY(mutex_);
+
+  // A link to the first broker this node connected to. If this link is broken,
+  // the node will lose all its other links too.
   mem::Ref<NodeLink> broker_link_ ABSL_GUARDED_BY(mutex_);
+
+  // Lookup table of broker-assigned node names and links to those nodes. All of
+  // these links and their associated names are received by the `broker_link_`
+  // if this is a non-broker node. If this is a broker node, these links are
+  // either assigned by this node itself, or received from other brokers in the
+  // system.
   absl::flat_hash_map<NodeName, mem::Ref<NodeLink>> node_links_
       ABSL_GUARDED_BY(mutex_);
+
+  // A map of other nodes to which this node is waiting for an introduction from
+  // `broker_link_`. Once such an introduction is received, all callbacks for
+  // that NodeName are executed.
   absl::flat_hash_map<NodeName, std::vector<EstablishLinkCallback>>
       pending_introductions_ ABSL_GUARDED_BY(mutex_);
 };
