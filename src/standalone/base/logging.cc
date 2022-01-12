6da27c370f000251e842ad8b2de8a9207220c5a7
Ken Rockot <rockot@google.com>
2022-01-11 18:47:34 -0800

    fix standalone build

--- src/standalone/base/logging.cc
+++ src/standalone/base/logging.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/logging.h"
+#include "src/standalone/base/logging.h"
 
 #include <atomic>
 #include <iostream>
